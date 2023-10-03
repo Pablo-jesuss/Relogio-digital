ff16fc765160c5bbcec04c8ed644d5727deff12a
Pablo-jesuss <pablo.programador26@gmail.com>
2023-10-02 22:58:10 -0300

    if

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const relogio = setInterval(function time() {
   let min = dateToday.getMinutes();
   let seg = dateToday.getSeconds();
 
+  if (hr < 10) hr = '0' + hr;
+
+  if (min< 10) min = '0' + min;
+
+  if (seg < 10) seg = '0' + seg;
+
+
+
   horas.textContent = hr;
   minutos.textContent = min;
   segundos.textContent = seg;
